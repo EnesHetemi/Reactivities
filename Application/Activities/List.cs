712314146ed41a6207f9f543ce2f49c6953b6689
EnesHetemi <eneshetemi3@gmail.com>
2022-07-26 23:24:12 +0200

    Dita e njembedhjete 26-07-2022

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,11 +1,10 @@
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
-using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using System.Linq;
 
 namespace Application.Activities
 {
@@ -15,18 +14,20 @@ namespace Application.Activities
 
         public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
+            private readonly IUserAccessor _userAccessor;
             private readonly IMapper _mapper;
             private readonly DataContext _context;
 
-            public Handler(DataContext context, IMapper mapper) 
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor) 
             {
+                _userAccessor = userAccessor;
                 _mapper = mapper;
                 _context = context;
             }
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activivities = await _context.Activities
-                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, new {currentUsername = _userAccessor.GetUsername()})
                     .ToListAsync(cancellationToken);
 
 
